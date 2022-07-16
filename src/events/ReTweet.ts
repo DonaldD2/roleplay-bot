d9eb123602e2160ee53c297f3e8c07cc33d38029
subtosharki <sahrkiyt@gmail.com>
2022-07-16 15:37:24 -0400

    is bot check for retweet

--- src/events/ReTweet.ts
+++ src/events/ReTweet.ts
@@ -4,7 +4,7 @@ import userModel from '../models/user.model';
 export = {
     name: 'messageReactionAdd',
     async execute(reaction: MessageReaction, user: User) {
-        if (reaction.emoji.id === '995421485063745706') {
+        if (reaction.emoji.id === '995421485063745706' && !user.bot) {
             const dbUser = await userModel.findOne({ discordId: user.id });
             const name = dbUser?.twitter?.username || user.username;
             reaction.message.channel.send({
