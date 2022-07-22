56e7204861060ad68943ebd329d8e6efc09d349d
subtosharki <sahrkiyt@gmail.com>
2022-07-21 21:15:04 -0400

    fix contact list

--- src/commands/iPhone.ts
+++ src/commands/iPhone.ts
@@ -327,11 +327,6 @@ export = {
                 const contactList = dbUser!.contacts!.map((contact) => {
                     if (contact.number != undefined) {
                         `${contact.name} - ${contact.number}`;
-                    } else {
-                        return interaction.reply({
-                            content: 'You have no contacts!',
-                            ephemeral: true,
-                        });
                     }
                 });
                 interaction.reply({
