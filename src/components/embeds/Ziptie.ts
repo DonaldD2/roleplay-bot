433471250787d51819fff0c636f1664e51895fbc
DonaldD2 <110241463+DonaldD2@users.noreply.github.com>
2022-08-03 23:25:15 +0100

    Create Ziptie.ts

---
+++ src/components/embeds/Ziptie.ts
@@ -0,0 +1,7 @@
+import { EmbedBuilder } from 'discord.js';
+
+export const Ziptying = new EmbedBuilder()
+    .setTimestamp()
+    .setDescription(`Applying Zipties...`);
+
+export const Ziptied = new EmbedBuilder().setTimestamp();
