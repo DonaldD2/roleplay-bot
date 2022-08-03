b39a8f79a758add0dbf134c1aebf275d244bcf2c
DonaldD2 <110241463+DonaldD2@users.noreply.github.com>
2022-08-03 21:57:33 +0100

    Create Drag.ts

---
+++ src/commands/Drag.ts
@@ -0,0 +1,37 @@
+import {
+    ChatInputCommandInteraction,
+    EmbedBuilder,
+    WebhookClient,
+    SlashCommandBuilder,
+    bold,
+    GuildMember,
+} from'discord.js';
+import ms from 'ms';
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('drag')
+    .setDescription('Drag someone!')
+    .addUserOption((option) =>
+        option
+        .setName('target')
+        .setDescription('Mention who you are dragging!')
+        .setRequired(true)
+    ),
+
+    async execute(interaction: ChatInputCommandInteraction) {
+        if (interaction.inCachedGuild()) {
+        const target = interaction.options.getMember('target');
+
+        if(!target) return interaction.reply({content: "⚠️ | You have not pinged anyone!", ephemeral: true})
+        const embed = new EmbedBuilder()
+        .setTimestamp()
+        .setColor('#2e85c5')    
+        .setDescription(`${bold(interaction.member?.nickname as unknown as string)} is dragging ${bold(interaction.options.getMember('target')?.nickname as unknown as string)}!`)
+
+        interaction.reply({embeds: [embed]})
+        interaction.channel.send({content: `${target}`})
+       
+    }},
+
+};
