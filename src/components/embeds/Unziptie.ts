c812f87f9c129fd3ef9828e46121a19411e25305
DonaldD2 <110241463+DonaldD2@users.noreply.github.com>
2022-08-03 23:24:53 +0100

    Create Unziptie.ts

---
+++ src/components/embeds/Unziptie.ts
@@ -0,0 +1,7 @@
+import { EmbedBuilder } from 'discord.js';
+
+export const unZiptyping = new EmbedBuilder()
+    .setTimestamp()
+    .setDescription(`Taking off zipties...`);
+
+export const unZiptied = new EmbedBuilder().setTimestamp();
